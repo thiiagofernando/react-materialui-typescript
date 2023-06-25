cacf5afe1501c3dbc6627aeb2680d542bb1433c9
Thiago Fernando <thiago.fernando@msn.com>
2023-06-25 18:37:24 -0400

    Configurando tema no MUI

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import { BrowserRouter } from "react-router-dom";
 import { AppRoutes } from "./routes";
+import { ThemeProvider } from "@emotion/react";
+import { LightThme } from './shared/themes/light';
 
 
 export const App = () => {
   return (
-    <BrowserRouter>
-      <AppRoutes/>
-    </BrowserRouter>
+    <ThemeProvider theme={LightThme}>
+      <BrowserRouter>
+        <AppRoutes/>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 };
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,18 @@
 import { Button } from "@mui/material";
-import { Routes,Route,Navigate } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 
-export const AppRoutes =()  =>{
-    return(
-        <Routes>
-            <Route path="/pagina-inicial" element={<Button color="success">Salvar</Button>} />
-            <Route path="*" element={<Navigate to="/pagina-inicial" />} />
-        </Routes>
-    );
-}
+export const AppRoutes = () => {
+  return (
+    <Routes>
+      <Route
+        path="/pagina-inicial"
+        element={
+          <Button variant="contained">
+            Salvar
+          </Button>
+        }
+      />
+      <Route path="*" element={<Navigate to="/pagina-inicial" />} />
+    </Routes>
+  );
+};
